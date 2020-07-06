07c8aa769358035a1ce3aaa57420d57b4782be3b
Victor Chris <victorchris51@gmail.com>
2020-07-07 00:17:52 +0300

    Added topic creation form

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 This a just a simple Django implementation of User Boards (Can be modified to do any boards of choice)
 
 Dependencies:
--Python: 3.8.2
--Django: 3.0.8
+-python: 3.8.2
+-django: 3.0.8
+-django-widget-tweaks: 1.4.8
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
 from django.http import HttpResponse
-from .models import Board
+from .forms import NewTopicForm
+from .models import Board, Topic, Post
 
 
 # Create your views here.
@@ -11,3 +13,27 @@ def home(request):
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first() # Get currently logged in user
+
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message = form.cleaned_data.get('message'),
+                topic = topic,
+                created_by = user
+            )
+            return redirect('board_topics', pk=board.pk) # Redirect to the created topic page
+    else:
+        form = NewTopicForm()
+
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+    
